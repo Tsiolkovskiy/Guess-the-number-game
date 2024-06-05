1c142f33fc779fcd7bef75dd45abbc59525d1b55
Tsiolkovskiy <56129993+Tsiolkovskiy@users.noreply.github.com>
2024-06-04 20:20:01 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -10,10 +10,8 @@
 // document.querySelector('.guess').value=23;
 // console.log(document.querySelector('.guess').value);
 
-const secretNumber = Math.trunc(Math.random()*20)+1
+let secretNumber = Math.trunc(Math.random()*20)+1;
 let score =20;
-
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function(){
     const guess=Number(document.querySelector('.guess').value);
@@ -23,6 +21,11 @@ document.querySelector('.check').addEventListener('click', function(){
         document.querySelector('.message').textContent="⛔ No Number!";
     }else if (guess ===secretNumber){
         document.querySelector('.message').textContent = '🎉Correct Number!';
+        document.querySelector('.number').textContent = secretNumber;
+
+        document.querySelector('body').style.backgroundColor='#60b347';
+        document.querySelector('.number').style.width='30rem';
+
     }else if(guess>secretNumber){
 
         if(score>1){
@@ -44,4 +47,17 @@ document.querySelector('.check').addEventListener('click', function(){
         document.querySelector('.score').textContent = 0;}
     }
     
+});
+
+
+document.querySelector('.again').addEventListener('click', function(){
+    score = 20;
+    secretNumber = Math.trunc(Math.random()*20)+1;
+    document.querySelector('.message').textContent = "Start guessing";
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector(".guess").value = '';
+
+    document.querySelector('body').style.backgroundColor='#222';
+    document.querySelector('.number').style.width='15rem';
 });
